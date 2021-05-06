f4187ef9f20ad806ffbfeda26b37dec6b8588861
KoredeDavid <koredeoluwashola@gmail.com>
2021-05-06 02:35:20 +0100

    Added responsiveness, redisigned the form

--- js/zuri.js
+++ js/zuri.js
@@ -1,5 +1,17 @@
 $(".learn").click(function(){   //id of the link which is being clicked
     $('html, body').animate({
-           scrollTop: $("#details").offset().top   //id of div to be scrolled
+           scrollTop: $("#learnmore").offset().top  
     }, 1000);
 });
+
+$(".dot").click(function(){
+    var x = document.getElementsByClassName("right")[0];
+    console.log(x.className)
+    if (x.className === "right") {
+        console.log('ok')
+        x.className += " responsive";
+    } else {
+        x.className = "right";
+        console.log(123)
+    }
+})
